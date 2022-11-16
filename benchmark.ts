adb3b7608ae9fa73a2707afa63af7780924c9626
Aimeric Seguin <aimeric.seguin@gmail.com>
2022-11-16 20:20:53 +0400

    🦀 Hotfixes and add benchmark

---
+++ benchmark.ts
@@ -0,0 +1,89 @@
+import {
+  pedersen,
+  starknet_pedersen,
+  starknet_pedersen_cairo,
+} from "./pkg/pedersen_wasm.js";
+import { toBufferLE } from "bigint-buffer";
+import { pedersen as starknetJsPedersen } from "starknet/dist/utils/hash";
+
+enum HashType {
+  PEDERSEN = "pedersen",
+  KECCAK = "keccak",
+}
+
+type Candidate = {
+  hashFn: any;
+  name: string;
+  type: HashType;
+};
+
+function randomUint32(): number {
+  return Math.floor(Math.random() * 4294967295);
+}
+
+function checkOutputs(candidate: Candidate) {
+  const { hashFn, name, type } = candidate;
+
+  const output = hashFn.call(null, "17", "71");
+
+  console.log(`${name} ${type} hash(17, 71): "${output}"`);
+}
+
+function doBenchmark(candidate: Candidate, inputs: number[][]) {
+  const { hashFn, name, type } = candidate;
+
+  const before = Date.now();
+  for (let i = 0; i < inputs.length; ++i) {
+    hashFn.call(null, inputs[i][0], inputs[i][1]);
+  }
+  const after = Date.now();
+
+  console.log(`${name} ${type} hash -> elapsed time: ${after - before}ms`);
+}
+
+function main() {
+  const candidates: Candidate[] = [
+    {
+      hashFn: (x: string, y: string) => starknetJsPedersen([x, y]),
+      name: "Starknet (JavaScript)  ",
+      type: HashType.PEDERSEN,
+    },
+    {
+      hashFn: (x: string, y: string) =>
+        starknet_pedersen(toBufferLE(BigInt(x), 32), toBufferLE(BigInt(y), 32)),
+      type: HashType.PEDERSEN,
+      name: "Geometry (Rust/WASM) v1",
+    },
+    {
+      hashFn: (x: string, y: string) =>
+        starknet_pedersen_cairo(
+          toBufferLE(BigInt(x), 32),
+          toBufferLE(BigInt(y), 32)
+        ),
+      type: HashType.PEDERSEN,
+      name: "Starknet Rust (Cairo)  ",
+    },
+    {
+      hashFn: (x: string, y: string) => pedersen(x, y),
+      type: HashType.PEDERSEN,
+      name: "Geometry (Rust/WASM) v2",
+    },
+  ];
+
+  candidates.forEach((candidate) => checkOutputs(candidate));
+
+  const iterations = [10, 100, 1000];
+
+  for (const iteration of iterations) {
+    const inputs = [];
+    for (let i = 0; i < iteration; ++i) {
+      inputs.push(randomUint32().toString(), randomUint32().toString());
+    }
+    console.log(`--> ${iteration} invocations <--`);
+    for (const candidate of candidates) {
+      doBenchmark(candidate, inputs);
+    }
+  }
+}
+
+main();
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ mod constants;
 mod error;
 mod pedersen;
 
+use std::str::FromStr;
+
 use ark_ff::{BigInteger, BigInteger256, FpParameters, FromBytes, PrimeField};
 use error::Error;
 use leftpad::left_pad_char;
@@ -26,21 +28,31 @@ pub fn field_element_from_be_hex(hex: &str) -> FieldElement {
     FieldElement::from_bytes_be(&buffer).unwrap()
 }
 
-// Computes and returns the pedersen hash taken from
+/// Computes the Starkware version of the Pedersen hash of x and y. All inputs are little-endian.
+/// Returns its 0x-prefixed lowercase hex representation. Output is big-endian.
 // https://github.com/geometryresearch/starknet-signatures/blob/722c5987cb96aee80f230a97fed685194c97b7db/packages/prover/src/pedersen.rs
 #[wasm_bindgen]
 pub fn pedersen(x: &str, y: &str) -> String {
-    let fa = field_element_from_be_hex(&left_pad_char(x, 64, '0')[0..64]);
-    let fb = field_element_from_be_hex(&left_pad_char(y, 64, '0')[0..64]);
+    let fqx = if !x.starts_with("0x") {
+        Fq::from_str(x).unwrap()
+    } else {
+        Fq::from_be_bytes_mod_order(
+            &field_element_from_be_hex(&left_pad_char(&x[2..], 64, '0')[0..64]).to_bytes_be(),
+        )
+    };
+    let fqy = if !y.starts_with("0x") {
+        Fq::from_str(y).unwrap()
+    } else {
+        Fq::from_be_bytes_mod_order(
+            &field_element_from_be_hex(&left_pad_char(&y[2..], 64, '0')[0..64]).to_bytes_be(),
+        )
+    };
 
-    let a = Fq::from_be_bytes_mod_order(&fa.to_bytes_be());
-    let b = Fq::from_be_bytes_mod_order(&fb.to_bytes_be());
+    let h = geo_pedersen_hash(&fqx, &fqy);
 
-    let h = geo_pedersen_hash(&a, &b);
     let mut formatted_hash = String::from("0x");
     formatted_hash.push_str(&h.into_repr().to_string());
-
-    formatted_hash
+    formatted_hash.to_lowercase()
 }
 
 pub fn bytes_safe<F: PrimeField<BigInt = BigInteger256>>(
@@ -60,19 +72,49 @@ pub fn bytes_safe<F: PrimeField<BigInt = BigInteger256>>(
     Ok(repr)
 }
 
-// Computes and returns the pedersen hash taken from starkware-crypto
+/// Computes the Starkware version of the Pedersen hash of x and y. All inputs are little-endian.
+/// Returns its 0x-prefixed lowercase hex representation. Output is big-endian.
+/// https://github.com/xJonathanLEI/starknet-rs/blob/89a724f00ba6000120b17f68f6da0b4c982eea2f/starknet-crypto/src/pedersen_hash.rs#L19
 #[wasm_bindgen]
-pub fn og_pedersen(x: Vec<u8>, y: Vec<u8>) -> String {
-    let x_repr = bytes_safe::<Fr>(&x).expect("err: x");
+pub fn starknet_pedersen(x: Vec<u8>, y: Vec<u8>) -> String {
+    let x_repr = bytes_safe::<Fr>(&x).unwrap();
 
     let xx: [u8; 32] = x_repr.to_bytes_be().try_into().unwrap();
 
-    let y_repr = bytes_safe::<Fr>(&y).expect("err: y");
+    let y_repr = bytes_safe::<Fr>(&y).unwrap();
     let yy: [u8; 32] = y_repr.to_bytes_be().try_into().unwrap();
 
     let fa = FieldElement::from_bytes_be(&xx).unwrap();
     let fb = FieldElement::from_bytes_be(&yy).unwrap();
 
     let h = pedersen_hash(&fa, &fb);
+
+    let mut formatted_hash = String::from("0x");
+    formatted_hash.push_str(
+        &Fq::from_str(&h.to_string())
+            .unwrap()
+            .into_repr()
+            .to_string(),
+    );
+    formatted_hash.to_lowercase()
+}
+
+/// Computes the Starkware version of the Pedersen hash of x and y. All inputs are big-endian.
+/// Returns its Cairo representation.
+/// https://github.com/xJonathanLEI/starknet-rs/blob/89a724f00ba6000120b17f68f6da0b4c982eea2f/starknet-crypto/src/pedersen_hash.rs#L19
+#[wasm_bindgen]
+pub fn starknet_pedersen_cairo(x: Vec<u8>, y: Vec<u8>) -> String {
+    let x_repr = bytes_safe::<Fr>(&x).unwrap();
+
+    let xx: [u8; 32] = x_repr.to_bytes_be().try_into().unwrap();
+
+    let y_repr = bytes_safe::<Fr>(&y).unwrap();
+    let yy: [u8; 32] = y_repr.to_bytes_be().try_into().unwrap();
+
+    let fa = FieldElement::from_bytes_be(&xx).unwrap();
+    let fb = FieldElement::from_bytes_be(&yy).unwrap();
+
+    let h = pedersen_hash(&fa, &fb);
+
     h.to_string()
 }
